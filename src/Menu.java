c3455d216b273cd15fd7e1352195ab2a7ebd968e
Greed <cristiancoltea97@gmail.com>
2022-11-15 17:15:22 +0100

    Creazione base Menu.

---
+++ src/Menu.java
@@ -0,0 +1,39 @@
+public class Menu{
+    public String nomeRistorante;
+    public String tipoMenu;
+    public String nomeMenu;
+
+    public Menu(String nomeRistorante, String tipoMenu, String nomeMenu) {
+        this.nomeRistorante = nomeRistorante;
+        this.tipoMenu = tipoMenu;
+        this.nomeMenu = nomeMenu;
+    }
+
+    public String getNomeRistorante() {
+        return nomeRistorante;
+    }
+
+    public void setNomeRistorante(String nomeRistorante) {
+        this.nomeRistorante = nomeRistorante;
+    }
+
+    public String getTipoMenu() {
+        return tipoMenu;
+    }
+
+    public void setTipoMenu(String tipoMenu) {
+        this.tipoMenu = tipoMenu;
+    }
+
+    public String getNomeMenu() {
+        return nomeMenu;
+    }
+
+    public void setNomeMenu(String nomeMenu) {
+        this.nomeMenu = nomeMenu;
+    }
+    public void printMenu(){
+        //array
+    }
+
+}
