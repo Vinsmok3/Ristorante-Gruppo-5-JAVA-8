9c565dbcd06efb48d8208077c7274941579dc6ae
Anthony Giordano <anthonygiordano1993@gmail.com>
2022-11-21 12:24:10 +0100

    Add Dolci

---
+++ src/Dolci.java
@@ -0,0 +1,21 @@
+public class Dolci {
+
+    public String dolce1;
+    public String dolce2;
+    public String dolce3;
+
+    public Dolci(String dolce1, String dolce2, String dolce3) {
+        this.dolce1 = dolce1;
+        this.dolce2 = dolce2;
+        this.dolce3 = dolce3;
+    }
+
+    @Override
+    public String toString() {
+        return "Dolci" +
+                "\n" + dolce1 +
+                "\n" + dolce2 +
+                "\n" + dolce3
+                ;
+    }
+}
--- src/Start.java
+++ src/Start.java
@@ -3,12 +3,15 @@ public class Start {
         Menu menu = new Menu("Ammutta Muddica","Pesce", "4 Uomini e un pesce");
         PrimiPiatti pesce = new PrimiPiatti("Pasta con le cozze", "Pasta allo scoglio", "Pasta con i gamberi");
         SecondiPiatti pesce2 = new SecondiPiatti("pepata di cozze", "gamberoni fritti", "frittura pesce mista");
+        Dolci dolci = new Dolci("Parfait di mandorle", "Tiramisù", "Bignè");
         Bevande bevande = new Bevande("Acqua", "Vino", "Birra");
         System.out.println(menu.completeMenu);
-        System.out.println("-------------------------------------");
+        System.out.println("--------------------------------------");
         System.out.println(pesce);
         System.out.println("--------------------------------------");
         System.out.println(pesce2);
+        System.out.println("--------------------------------------");
+        System.out.println(dolci);
         System.out.println("--------------------------------------");
         System.out.println(bevande);
     }
