787886061c7a1af9fb83a1919a87fdf7e0eb2804
Anthony Giordano <anthonygiordano1993@gmail.com>
2022-11-24 18:38:38 +0100

    Feature: added dolci to start

--- src/Start.java
+++ src/Start.java
@@ -10,10 +10,15 @@ public class Start {
         Portata primo1 = new PrimiPiatti("Spaghetti", "Pasta con le cozze", 23,true);
         Portata primo2 = new PrimiPiatti("Penne","Pasta con le vongole",25,true);
         Portata primo3 = new PrimiPiatti("Farfalle", "Farfalle con il salmone",18,false);
+        Portata dolce1 = new Dolci("Semi-freddo", "Parfait di mandorle", 10, true);
+        Portata dolce2 = new Dolci("A freddo", "Tiramisù", 22, true);
+
         Cliente cliente1 = new Cliente("Paul","Cannon",333333338, true);
         menu.addPrimi(primo1);
         menu.addPrimi(primo2);
         menu.addPrimi(primo3);
+        menu.addDolci(dolce1);
+        menu.addDolci(dolce2);
         menu.addBevanda(bevanda1);
         menu.addBevanda(bevanda2);
         menu.printAllMenu();
