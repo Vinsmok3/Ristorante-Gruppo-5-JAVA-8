376e59567c44185bff62d2ccd8cc03dc57b1c0b5
Emilino <giusysch3@libero.it>
2022-11-24 18:21:37 +0100

    refact: Classes PrimiPiatti

--- src/PrimiPiatti.java
+++ src/PrimiPiatti.java
@@ -1,46 +1,25 @@
 
+public class PrimiPiatti extends Portata{
+    private boolean glutenFree;
 
-public class PrimiPiatti {
-    public String portata1;
-    public String portata2;
-    public String portata3;
+    public PrimiPiatti(String type, String name, Integer price) {
+        super(type, name, price);
+    }
+    public PrimiPiatti(String type, String name, Integer price, Boolean hasGluten) {
+        super(type, name, price);
+        glutenFree = hasGluten;
 
-    public PrimiPiatti(String portata1, String portata2, String portata3) {
-        this.portata1 = portata1;
-        this.portata2 = portata2;
-        this.portata3 = portata3;
     }
 
     @Override
-    public String toString() {
-        return "Primi Piatti" +
-                "\n" + portata1  +
-                "\n" + portata2 +
-                "\n" + portata3
-                ;
+    public String toString(){
+        return "Tipo: " + getType() + " - " + "Nome: " + getName() + " - " + "Prezzo: " + getPrice() + " - " + hasGlutenFree();
     }
-
-    public String getPortata1() {
-        return portata1;
-    }
-
-    public void setPortata1(String portata1) {
-        this.portata1 = portata1;
-    }
-
-    public String getPortata2() {
-        return portata2;
-    }
-
-    public void setPortata2(String portata2) {
-        this.portata2 = portata2;
-    }
-
-    public String getPortata3() {
-        return portata3;
-    }
-
-    public void setPortata3(String portata3) {
-        this.portata3 = portata3;
+    public String hasGlutenFree(){
+        if (glutenFree){
+            return "Contiene il glutine";
+        }else {
+            return "Senza glutine";
+        }
     }
 }
--- src/Start.java
+++ src/Start.java
@@ -7,9 +7,13 @@ public class Start {
 
         Portata bevanda1 = new Bevande("Vino", "Tavernello",20,true);
         Portata bevanda2 = new Bevande("Acqua", "Levissima", 10,false);
-
+        Portata primo1 = new PrimiPiatti("Spaghetti", "Pasta con le cozze", 23,true);
+        Portata primo2 = new PrimiPiatti("Penne","Pasta con le vongole",25,true);
+        Portata primo3 = new PrimiPiatti("Farfalle", "Farfalle con il salmone",18,false);
         Cliente cliente1 = new Cliente("Paul","Cannon",333333338, true);
-
+        menu.addPrimi(primo1);
+        menu.addPrimi(primo2);
+        menu.addPrimi(primo3);
 
         menu.addBevanda(bevanda1);
         menu.addBevanda(bevanda2);
@@ -17,7 +21,5 @@ public class Start {
 
         listClienti.addClient(cliente1);
         listClienti.printAllClients();
-
-
     }
 }
